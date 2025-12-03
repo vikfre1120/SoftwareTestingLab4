81ddeb1f71d7525ac32e302a87c76be892d658cc
Viktor Fredriksson <Vikfred@hotmail.com>
2025-12-03 09:59:35 +0100

    testar nu

---
+++ Search.java
@@ -0,0 +1,20 @@
+class Search {
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid; // Target found
+            } else if (arr[mid] < target) {
+                left = mid + 1; // Search in the right half
+            } else {
+                right = mid - 1; // Search in the left half
+            }
+        }
+
+        return -1; // Target not found
+    }
+}   
