380daa6e8e03a4e7e53df60aa760d01358951ff2
Viktor Fredriksson <Vikfred@hotmail.com>
2025-12-03 10:12:23 +0100

    first cmmin

--- Search.java
+++ Search.java
@@ -3,18 +3,6 @@ class Search {
         int left = 0;
         int right = arr.length - 1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (arr[mid] == target) {
-                return mid; // Target found
-            } else if (arr[mid] < target) {
-                left = mid + 1; // Search in the right half
-            } else {
-                right = mid - 1; // Search in the left half
-            }
-        }
-
         return -1; // Target not found
     }
 }   
