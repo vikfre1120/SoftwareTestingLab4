6a54c08804c619d278f639a797baef0dbb41fb54
Augusta <au8037sa-s@lu.se>
2025-12-03 11:12:32 +0100

    This works for sure

--- Search.java
+++ Search.java
@@ -5,12 +5,18 @@ class Search {
     public static void main(String[] args) {
         String searchString = args[0];
         String filePath = args[1];
+        System.out.println(filePath);
         try {
             BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            
             String line = reader.readLine();
-            if (line.contains(searchString)) {
+            while(line != null){
+                if (line.contains(searchString)) {
                 System.out.println(line);
+            } 
+            line = reader.readLine();
             }
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
